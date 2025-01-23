71bc8234a65de1727f96438f83e509ddf7dbff9a
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-01-24 00:33:49 +0100

    Fixed data type

--- model/mockuptemplates.go
+++ model/mockuptemplates.go
@@ -1,13 +1,14 @@
 package model
 
 // Note: Dimensions should be int as declared in openapi.json, but the actual API send floats
+// BackgroundColor should be int but is actually a string
 type MockupTemplates struct {
 	CatalogVariantIDs   []int   `json:"catalog_variant_ids" bson:"catalog_variant_ids"`
 	Placement           string  `json:"placement" bson:"placement"`
 	Technique           string  `json:"technique" bson:"technique"`
 	ImageURL            string  `json:"image_url" bson:"image_url"`
 	BackgroundURL       string  `json:"background_url" bson:"background_url"`
-	BackgroundColor     int     `json:"background_color" bson:"background_color"`
+	BackgroundColor     string  `json:"background_color" bson:"background_color"`
 	PrintfileID         int     `json:"printfile_id" bson:"printfile_id"`
 	TemplateWidth       float64 `json:"template_width" bson:"template_width"`
 	TemplateHeight      float64 `json:"template_height" bson:"template_height"`
