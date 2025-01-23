3964a9e1d6c61db6a0847e048d8b73a9684b2137
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-01-24 00:16:15 +0100

    Removed old templates

--- model/responses/templates.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import model "github.com/baldurstod/go-printful-sdk/model"
-
-// TODO: adapt for api v2
-type TemplatesResponse struct {
-	Code   int                    `json:"code" bson:"code" mapstructure:"code"`
-	Result model.ProductTemplates `json:"result" bson:"result" mapstructure:"result"`
-}
