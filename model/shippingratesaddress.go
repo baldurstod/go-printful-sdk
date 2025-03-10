a8fa6f45928e87b5609c9e60e898dd81ae47fda9
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-10 23:17:50 +0100

    Added mapstructure tags

--- model/shippingratesaddress.go
+++ model/shippingratesaddress.go
@@ -1,10 +1,10 @@
 package model
 
 type ShippingRatesAddress struct {
-	Address1    string `json:"address1" bson:"address1"`
-	Address2    string `json:"address2" bson:"address2"`
-	City        string `json:"city" bson:"city"`
-	StateCode   string `json:"state_code" bson:"state_code"`
-	CountryCode string `json:"country_code" bson:"country_code"`
-	ZIP         string `json:"zip" bson:"zip"`
+	Address1    string `json:"address1" bson:"address1" mapstructure:"address1"`
+	Address2    string `json:"address2" bson:"address2" mapstructure:"address2"`
+	City        string `json:"city" bson:"city" mapstructure:"city"`
+	StateCode   string `json:"state_code" bson:"state_code" mapstructure:"state_code"`
+	CountryCode string `json:"country_code" bson:"country_code" mapstructure:"country_code"`
+	ZIP         string `json:"zip" bson:"zip" mapstructure:"zip"`
 }
