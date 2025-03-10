00ababc5310f84419eed7e2c91f4c215c4b4fea4
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-11 00:10:29 +0100

    CatalogOrWarehouseShippingRateItem: added mapstructure tags

--- model/catalogorwarehouseshippingrateitem.go
+++ model/catalogorwarehouseshippingrateitem.go
@@ -1,8 +1,8 @@
 package model
 
 type CatalogOrWarehouseShippingRateItem struct {
-	Source             string `json:"source" bson:"source"`
-	Quantity           int    `json:"quantity" bson:"quantity"`
-	CatalogVariantID   int    `json:"catalog_variant_id" bson:"catalog_variant_id"`
-	WarehouseVariantID int    `json:"warehouse_variant_id" bson:"warehouse_variant_id"`
+	Source             string `json:"source" bson:"source" mapstructure:"source"`
+	Quantity           int    `json:"quantity" bson:"quantity" mapstructure:"quantity"`
+	CatalogVariantID   int    `json:"catalog_variant_id" bson:"catalog_variant_id" mapstructure:"catalog_variant_id"`
+	WarehouseVariantID int    `json:"warehouse_variant_id" bson:"warehouse_variant_id" mapstructure:"warehouse_variant_id"`
 }
