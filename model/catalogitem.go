a2095c5fcdde849238691b4928b1c96aaa2a7d19
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-14 00:32:42 +0100

    Added missing tags

--- model/catalogitem.go
+++ model/catalogitem.go
@@ -1,9 +1,9 @@
 package model
 
 type CatalogItem struct {
-	Source           string `json:"source" bson:"source"`
-	CatalogVariantID int    `json:"catalog_variant_id" bson:"catalog_variant_id"`
-	Item
+	Source           string `json:"source" bson:"source" mapstructure:"source"`
+	CatalogVariantID int    `json:"catalog_variant_id" bson:"catalog_variant_id" mapstructure:"catalog_variant_id"`
+	Item             `json:"item" bson:"item" mapstructure:"item"`
 }
 
 func NewCatalogItem() CatalogItem {
