a69b4b97b00fd1ef02de4f3c48571b45e02aa00a
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-14 15:40:03 +0100

    Added missing tags

--- model/layer.go
+++ model/layer.go
@@ -1,24 +1,24 @@
 package model
 
 type Layer struct {
-	Type           string `json:"type" bson:"type"`
-	Url            string `json:"url" bson:"url"`
-	*LayerOptions  `json:"layer_options" bson:"layer_options"`
-	*LayerPosition `json:"position" bson:"position"`
+	Type           string `json:"type" bson:"type" mapstructure:"type"`
+	Url            string `json:"url" bson:"url" mapstructure:"url"`
+	*LayerOptions  `json:"layer_options,omitempty" bson:"layer_options" mapstructure:"layer_options,omitempty"`
+	*LayerPosition `json:"position,omitempty" bson:"position" mapstructure:"position,omitempty"`
 }
 
 type LayerOptions []LayerOption
 
 type LayerOption struct {
-	Name       string   `json:"name" bson:"name"`
-	Techniques []string `json:"techniques" bson:"techniques"`
-	Type       string   `json:"type" bson:"type"`
-	Values     any      `json:"values" bson:"values"`
+	Name       string   `json:"name" bson:"name" mapstructure:"name"`
+	Techniques []string `json:"techniques" bson:"techniques" mapstructure:"techniques"`
+	Type       string   `json:"type" bson:"type" mapstructure:"type"`
+	Values     any      `json:"values" bson:"values" mapstructure:"values"`
 }
 
 type LayerPosition struct {
-	Width  float64 `json:"width" bson:"width"`
-	Height float64 `json:"height" bson:"height"`
-	Top    float64 `json:"top" bson:"top"`
-	Left   float64 `json:"left" bson:"left"`
+	Width  float64 `json:"width" bson:"width" mapstructure:"width"`
+	Height float64 `json:"height" bson:"height" mapstructure:"height"`
+	Top    float64 `json:"top" bson:"top" mapstructure:"top"`
+	Left   float64 `json:"left" bson:"left" mapstructure:"left"`
 }
--- model/placement.go
+++ model/placement.go
@@ -1,22 +1,22 @@
 package model
 
 type Placement struct {
-	Placement         string  `json:"placement" bson:"placement"`
-	Technique         string  `json:"technique" bson:"technique"`
-	PrintAreaType     string  `json:"print_area_type" bson:"print_area_type"`
-	Layers            []Layer `json:"layers" bson:"layers"`
-	PlacementOptions  `json:"placement_options" bson:"placement_options"`
-	Status            string `json:"status" bson:"status"`
-	StatusExplanation string `json:"status_explanation" bson:"status_explanation"`
+	Placement         string  `json:"placement" bson:"placement" mapstructure:"placement"`
+	Technique         string  `json:"technique" bson:"technique" mapstructure:"technique"`
+	PrintAreaType     string  `json:"print_area_type" bson:"print_area_type" mapstructure:"print_area_type"`
+	Layers            []Layer `json:"layers" bson:"layers" mapstructure:"layers"`
+	PlacementOptions  `json:"placement_options" bson:"placement_options" mapstructure:"placement_options"`
+	Status            string `json:"status" bson:"status" mapstructure:"status"`
+	StatusExplanation string `json:"status_explanation" bson:"status_explanation" mapstructure:"status_explanation"`
 }
 
 type PlacementOptions []PlacementOption
 
 type PlacementOption struct {
-	Name       string   `json:"name" bson:"name"`
-	Techniques []string `json:"techniques" bson:"techniques"`
-	Type       string   `json:"type" bson:"type"`
-	Values     any      `json:"values" bson:"values"`
+	Name       string   `json:"name" bson:"name" mapstructure:"name"`
+	Techniques []string `json:"techniques" bson:"techniques" mapstructure:"techniques"`
+	Type       string   `json:"type" bson:"type" mapstructure:"type"`
+	Values     any      `json:"values" bson:"values" mapstructure:"values"`
 }
 
 func NewPlacement() Placement {
